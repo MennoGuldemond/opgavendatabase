362a4ca62f1ef839142bb3a45b7d0c4703048999
Menno Guldemond <mennoguldemond@live.nl>
2025-04-14 22:11:19 +0200

    wip

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,23 +1,28 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { QuillModule } from 'ngx-quill';
 import renderMathInElement from 'katex/contrib/auto-render';
+import { jsPDF } from 'jspdf';
 
 import * as katex from 'katex';
-
 (window as any).katex = katex;
 
 @Component({
   selector: 'app-editor',
-  imports: [CommonModule, QuillModule, ReactiveFormsModule],
+  imports: [CommonModule, QuillModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss',
 })
 export class EditorComponent implements AfterViewInit {
-  editorControl = new FormControl('This is a math expression: $x^2 + y^2 = z^2$');
+  editorControl: FormControl;
 
-  ngAfterViewInit(): void {
+  ngOnInit() {
+    this.editorControl = new FormControl('This is a math expression: $x^2 + y^2 = z^2$');
+  }
+
+  ngAfterViewInit() {
     this.renderMath();
   }
 
@@ -36,5 +41,24 @@ export class EditorComponent implements AfterViewInit {
 
   onContentChanged() {
     this.renderMath();
+  }
+
+  save() {
+    const content = this.editorControl.value;
+
+    // Check if content is available
+    if (!content || typeof content !== 'string') {
+      console.error('Editor content is empty or invalid.');
+      return;
+    }
+
+    // Create a new instance of jsPDF
+    const doc = new jsPDF();
+
+    // Add the editor content to the PDF
+    doc.text(content, 10, 10);
+
+    // Save the generated PDF
+    doc.save('generated.pdf');
   }
 }
